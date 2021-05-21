258bb20535a36b3abe68bcc9d1097343d8a84663
Student <student@PNCLB32-13.pnc.lan>
2021-05-22 01:24:01 +0700

    add project

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 
@@ -10,3 +8,22 @@ app.use(express.urlencoded());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send("Hello Project"))
+
+let users = [
+    {username: "sophorn", password : "007", color: "red", text: "i want to sleep"},
+    {username: "sreytouch", password : "777777", color: "green", text: "wooooooooo"},
+
+]
+
+app.get('/users', (req , res) =>{
+    res.send(users);
+})
+
+app.post('/users', (req, res) =>{
+    
+    let user = {text:req.body.text};
+    users.push(user);
+    res.send(users);
+
+
+})
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,70 @@
+//display item on broswer//
+function displayUser(response){
+    let users = response.data;
+    const text = document.querySelector("#inputId");
+    let storeOfNewUser = document.querySelector(".store");
+    const listOfUser = document.querySelector(".listOfUser");
+    
+    if (listOfUser!==null){
+        listOfUser.remove();
+    }
+    const newOfuser = document.createElement("div");
+    newOfuser.classList.add("listOfUser");
+    for (let user of users){
 
+        //fieldset contain all span//
+        const fieldset = document.createElement("fieldset");
+        
+
+        //contain text message name and //
+        const spanOfInput =document.createElement("span");
+        spanOfInput.textContent = user.username + ": " + user.text;
+
+        //contain icon quote//
+        const spanOfquote = document.createElement('span');
+        spanOfquote.className = "quote-left fa fa-quote-left hvr-grow";
+
+        //contain icon eidt//
+        const spanOfEdit = document.createElement("span");
+        spanOfEdit.className ="edit fa fa-pencil-square-o hvr-grow";
+
+        //append all spans to  fieldset//
+        fieldset.appendChild(spanOfquote);
+        fieldset.appendChild(spanOfEdit);
+        fieldset.appendChild(spanOfInput);
+        
+        // fieldset.appendChild(span_delete);
+        
+        fieldset.style.backgroundColor = user.color;
+        newOfuser.appendChild(fieldset);
+        storeOfNewUser.appendChild(newOfuser);
+    }
+
+
+    text.value = "";
+
+}
+
+
+
+
+
+
+function sendMessage(e){
+    const text = document.querySelector("#inputId").value;
+    let user = {text: text};
+    const url = "http://localhost:5000/users";
+    axios.post(url, user).then(displayUser);
+
+
+}
+const btnsend = document.querySelector("#submit");
+btnsend.addEventListener('click', sendMessage);
+
+
+
+function loadData(){
+    const url = "http://localhost:5000/users";
+    axios.get(url).then(displayUser);
+}
+loadData();
